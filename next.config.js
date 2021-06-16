7db4fbc5d9ad42ee8552dbaa040d49452409c93e
austins <austins@users.noreply.github.com>
2021-06-16 01:23:38 -0700

    Allow API host domain with image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     images: {
-        domains: [new URL(process.env.NEXT_PUBLIC_SITE_URL).host, 'secure.gravatar.com'],
+        domains: [
+            new URL(process.env.NEXT_PUBLIC_SITE_URL).host,
+            new URL(process.env.API_GRAPHQL_URL).host,
+            'secure.gravatar.com',
+        ],
     },
     webpack: config => {
         config.module.rules.push({
