1ddb25957ed0dc6e7ecf0832988cda8254c0ba35
austins <austins@users.noreply.github.com>
2021-07-11 02:43:56 -0700

    Post content not needed for postsQuery

--- src/lib/data/queries.js
+++ src/lib/data/queries.js
@@ -157,7 +157,6 @@ export const postsQuery = gql`
                     dateGmt
                     title
                     excerpt
-                    content
                     author {
                         node {
                             name
